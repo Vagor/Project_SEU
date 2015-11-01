9101a9f5c5af1b4ea32865d80da9e965ccdecfef
Vagor <vagor.tao@gmail.com>
2015-11-01 22:00:55 +0800

    更新again

--- seu2/php/onloadSocial.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	header("Content-Type:text/plain;charset=utf-8");
-	include_once 'fun.inc.php';
-	linkDB();
-	echo $_POST["id"];
-	$data = onloadSocial($_POST["id"]);
-	echo json_encode($data,JSON_UNESCAPED_UNICODE);
-?>
