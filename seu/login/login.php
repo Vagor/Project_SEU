417282e82ac2718117e40e06080c5dea73774251
Vagor <vagor.tao@gmail.com>
2015-11-02 14:55:26 +0800

    解决了轮播图问题

--- seu/login/login.php
+++ seu/login/login.php
@@ -2,13 +2,11 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="./things/css" rel="stylesheet" type="text/css">
-<link data-turbolinks-track="true" href="./things/bootstrap.min.css" media="all" rel="stylesheet">
+<link href="./things/mine.css" rel="stylesheet" type="text/css">
 <link data-turbolinks-track="true" href="./things/local.css" media="all" rel="stylesheet">
-<script data-turbolinks-track="true" src="./things/jquery-1.11.1.js"></script>
-<script data-turbolinks-track="true" src="./things/bootstrap.js"></script>
-<script data-turbolinks-track="true" src="./things/analytics.js"></script>
-
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 
 <body class="">
@@ -57,13 +55,6 @@
               <div class="col-md-1"></div>
               <div class="col-md-10">
                 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                  <!-- Indicators -->
-                <ol class="carousel-indicators">
-                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                </ol>
-
                   <!-- Wrapper for slides -->
                   <div class="carousel-inner " role="listbox">
                     <div class="item active">
@@ -173,16 +164,16 @@
                       </div>
                     </div>
                   </div>
-
-                  <!-- Controls -->
-                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                  <!-- Controls -->                
+                  <a class="carousel-control left" style="background-image: linear-gradient(to right,rgba(0,0,0,0),rgba(0,0,0,0) 100%);}" href="#carousel-example-generic" role="button" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                   </a>
-                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                  <a class="carousel-control right" href="#carousel-example-generic" style="background-image: linear-gradient(to right,rgba(0,0,0,0),rgba(0,0,0,0) 100%);}" role="button" data-slide="next">
                     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
                   </a>
+
                 </div>
               </div>
               <div class="col-md-1"></div>
